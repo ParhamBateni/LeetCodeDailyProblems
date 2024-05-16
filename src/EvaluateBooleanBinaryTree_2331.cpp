091aed5d2896748a20a3e7ceb77e49451db4bd77
Parham Bateni <m.bateni@student.tudelft.nl>
2024-05-16 22:16:31 +0200

    Added 16/05/2024 problem

---
+++ src/EvaluateBooleanBinaryTree_2331.cpp
@@ -0,0 +1,60 @@
+/*
+You are given the root of a full binary tree with the following properties:
+
+    Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+    Non-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+
+The evaluation of a node is as follows:
+
+    If the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+    Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+
+Return the boolean result of evaluating the root node.
+
+A full binary tree is a binary tree where each node has either 0 or 2 children.
+
+A leaf node is a node that has zero children.*/
+
+#include "../utils.h"
+#include "gtest/gtest.h"
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution2331 : public ::testing::Test {
+public:
+    static bool evaluateTree(TreeNode *root) {
+        if (root->val == 0)
+            return false;
+        else if (root->val == 1)
+            return true;
+        else if (root->val == 2)
+            return evaluateTree(root->left) || evaluateTree(root->right);
+        else
+            return evaluateTree(root->left) && evaluateTree(root->right);
+    }
+};
+
+
+TEST(Solution2331, T1) {
+    TreeNode t1 = TreeNode(1);
+    TreeNode t2 = TreeNode(0);
+    TreeNode t3 = TreeNode(3, &t1, &t2);
+    TreeNode t4 = TreeNode(1);
+    TreeNode t5 = TreeNode(2, &t4, &t3);
+    ASSERT_TRUE(Solution2331::evaluateTree(&t5));
+}
+
+TEST(Solution2331, T2) {
+    TreeNode t1 = TreeNode(0);
+    ASSERT_FALSE(Solution2331::evaluateTree(&t1));
+}
